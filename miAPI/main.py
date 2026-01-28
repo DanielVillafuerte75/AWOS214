99a2fa0a2baeb47dd8b83b824a50f22e919a1634
DanielVillafuerte75 <vdanielisaac@gmail.com>
2026-01-27 20:08:02 -0600

    Create main.py

---
+++ miAPI/main.py
@@ -0,0 +1,19 @@
+#Zona de Importaciones
+from fastapi import FastAPI
+import asyncio
+
+#Zona de Instancias del servidor
+app = FastAPI()
+
+#Zona de Endpoints
+@app.get("/")
+async def bienvenida():
+    return {"mensaje": "!Bienvenido a mi API!"}
+
+@app.get("/HolaMundo")
+async def hola():
+    await asyncio.sleep(4) #simulacion de una peticion
+    return {
+        "mensaje": "!Hola Mundo FastAPI!",
+        "estatus":"200"
+        }
