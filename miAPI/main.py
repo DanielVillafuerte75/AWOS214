db7f52290f1b4d79f04b43930e60b4018432eab1
DanielVillafuerte75 <vdanielisaac@gmail.com>
2026-02-10 21:28:14 -0600

    Practica 2. Documentacion automatica
    Se agregaron parametros para validacion. inputs obligatorios para id

--- miAPI/main.py
+++ miAPI/main.py
@@ -1,19 +1,46 @@
 #Zona de Importaciones
 from fastapi import FastAPI
 import asyncio
+from typing import Optional
 
 #Zona de Instancias del servidor
-app = FastAPI()
+app = FastAPI(
+    title="Mi primer API", 
+    description= "Esta api esta creada por Daniel Villafuerte",
+    version="1.0.0"
+    )
+
+#TB ficticia
+usuarios=[
+        {"id":1, "nombre":"Juan","edad":21},
+        {"id":2, "nombre":"Israel","edad":21},
+        {"id":3, "nombre":"Sofi","edad":21},
+    ]
 
 #Zona de Endpoints
-@app.get("/")
+@app.get("/",tags=["Inicio"])
 async def bienvenida():
     return {"mensaje": "!Bienvenido a mi API!"}
 
-@app.get("/HolaMundo")
+@app.get("/HolaMundo", tags=["Bienvenida Asincrona"])
 async def hola():
-    await asyncio.sleep(4) #simulacion de una peticion
+    await asyncio.sleep(3) #simulacion de una peticion
     return {
         "mensaje": "!Hola Mundo FastAPI!",
         "estatus":"200"
         }
+@app.get("/v1/usuario/{id}",tags=["Parametro Obligatorio"])
+async def consultaUno(id:int):
+    return {"Se encontro usuario": id}
+
+
+
+@app.get("v1/usuarios/",tags=["Parametro Opcional"])
+async def consultaTodos(id:Optional[int]=None):
+    if id is not None:
+        for usuariok in usuarios:
+            if usuariok ["id"] == id:
+                return{"mensaje": "usuario encontrado", "usuario": usuariok}
+            return {"mensaje": "usuario no encontrado", "usuario":id}
+        else:
+                return {"mensaje": "No se proporciono id"}
